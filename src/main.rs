0ec73ae69ad160f3910a144e104c2c5b411373ae
Asitha Rodrigo <asitha@mac.com>
2024-05-25 13:59:49 +0100

    some static variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+const OUR_COURSE: &str = "Rust with AutGPT";
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to this course on {}", OUR_COURSE);
+
+    //Stack
+    let x: i32;
+    x = 42;
+    println!("x is {}", x);
+
+    let y: i32 = 4;
+    println!("y is {}", y);
+
+    //for loop
+    for i in 0..=y {
+        if i != 4 {
+            println!("i is {}", i);
+        } else {
+            println!("i is {}", i);
+        }
+    }
+
+    //Mutability
+    let mut z: i32 = 5;
+    print!("Z was {} ", z);
+    z = 10;
+    println!("Z is now {}", z);
 }
